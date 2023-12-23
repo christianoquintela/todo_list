3a17dce481aa5582db5e121356ee5b7a8d469780
Christiano Quintela <christianoquintela@gmail.com>
2023-12-22 22:06:38 -0300

    Todo list, iniciando em Vite + React

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+//Libs
+// import { useState } from 'react';
+
+//Css
+import classes from './App.module.css';
+
+function App() {
+  return <div className={classes.container_App}></div>;
+}
+
+export default App;
