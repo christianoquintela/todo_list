41bc1ea6906dbdd72c16b5418fdf9318eb5943c8
Christiano Quintela <christianoquintela@gmail.com>
2023-12-27 00:35:18 -0300

    Continuar daqui, erro ao tentar usar if ternário com css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 //Libs
-// import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 //Components
 import Card from './components/card/card';
@@ -7,16 +7,45 @@ import Card from './components/card/card';
 //Css
 import classes from './App.module.css';
 
+export type Todo = {
+  id: number;
+  title?: string;
+  completed?: boolean;
+};
+
 function App() {
+  const [todoInput, setTodoInput] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  function addTodo() {
+    setTodos((previousTodos) => [
+      ...todos,
+      { id: Math.random(), title: todoInput, completed: false },
+    ]);
+    setTodoInput('');
+  }
+  function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+    setTodoInput(e.target.value);
+  }
+
+  function completeTodo(id: number) {
+    setTodos((previousTodos) => previousTodos.map((todo) => todo.id !== id ? todo : {...todo, completed: !todo.completed}));
+    
+  }
+
   return (
     <div className={classes.container_App}>
-
       <div className={classes.add_todo}>
-        <input placeholder='Fazer café' />
-        <button>Adicionar</button>
-        <Card title='fazer café' />
+        <input
+          placeholder='Fazer, faça, prática, praticar, praticou!'
+          value={todoInput}
+          onChange={handleInputChange}
+        />
+        <button onClick={addTodo}>Adicionar</button>
+        {todos.map((todo) => (
+          <Card key={todo.id} todo={todo} completeTodo={completeTodo}/>
+        ))}
       </div>
-
     </div>
   );
 }
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,20 +1,27 @@
 //Css
+import { Todo } from '../../App';
 import classes from './card.module.css';
 
 //Interfaces
 type CardProps = {
-  title: string;
+  todo: Todo;
+  completeTodo: (id: number) => void;
 };
 
 //Function
-export default function Card({ title }: CardProps) {
+export default function Card({ todo, completeTodo }: CardProps) {
+  function handleCompleteTodo() {
+    completeTodo(todo.id);
+  }
+
   return (
-    <div className={classes.container_Card}>
+    //defeito ao passar uma classe em css module com if ternário
+    <div className={`${classes.container_Card} ${classes.done}`}>
       {/* <h2>Fazer café</h2> */}
-      <h1>{title}</h1>
+      <h1>{todo.title}</h1>
 
       <div className={classes.card_buttons}>
-        <button>Completar</button>
+        <button onClick={handleCompleteTodo}>Completar</button>
         <button>Deletar</button>
       </div>
     </div>
